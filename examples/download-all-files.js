5c21b91bd2f35c4af2a04a05feb1be57759c7e28
Geovane Schmitz <geovane963hyg@gmail.com>
2019-05-24 06:25:43 -0300

    Adicionado metodo para download  refatoração de codigo

--- examples/download-all-files.js
+++ examples/download-all-files.js
@@ -1,23 +1,22 @@
 const Sigaa = require ('..');
-const https = require ('https');
 const fs = require ('fs');
-const uuid = require('uuid/v4');
 const path = require ('path');
-const querystring = require ('querystring');
 
 const sigaa = new Sigaa ();
 
 
 // put your crendecias
-var userName = '';
-var password = '';
+var userName = 'geovane.s06';
+var password = '33461334gG';
 
 
 
 //this creates folder downloads
 let BaseDestiny = path.join (
-  'downloads',
+  '.',
+  'downloads'
 );
+
 fs.mkdir(BaseDestiny, err => { 
   if (err && err.code != 'EEXIST') throw 'up'
 })
@@ -48,10 +47,10 @@ sigaa.account
           token
         ); //this lists all topics
         for (let topic of topics) { //for each topic
-          for (let attachment of topic.attachments) { 
+          let attachments = topic.attachments
+          for (let attachment of attachments) { 
             if (attachment.type == 'file') {
-              console.log(`> ${attachment.name}`)
-              await downloadFile(studentClass, attachment);         
+              await attachment.downloadFile(BaseDestiny);         
             }
           }
         }
@@ -73,55 +72,3 @@ sigaa.account
     console.log (data);
   });
   
-
-async function downloadFile(studentClass, attachment) {
-  await new Promise((resolve, reject) => {
-
-    fs.mkdir(path.join(BaseDestiny, studentClass.name), err => { // this creates path with class name
-      if (err && err.code != 'EEXIST')
-        throw 'up';
-
-      let tmpFileDestiny = path.join(BaseDestiny, studentClass.name, uuid());
-      let file = fs.createWriteStream(tmpFileDestiny);
-
-      let link = new URL(attachment.form.action);
-      
-      //http options
-      const options = {
-        hostname: link.hostname,
-        port: 443,
-        path: link.pathname + link.search,
-        method: 'POST',
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0',
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Cookie': token
-        },
-      };
-      
-      // this converts post parameters to string
-      let postOptionsString = querystring.stringify(attachment.form.postOptions);
-      // this inserts post parameters length to  header http
-      options.headers['Content-Length'] = Buffer.byteLength(postOptionsString);
-     
-      // makes request
-      var request = https.request(options, (response) => {
-        let filename = response.headers['content-disposition'].replace(/([\S\s]*?)filename=\"/gm, '').slice(0, -1);
-        let filepath = path.join(BaseDestiny, studentClass.name, filename)
-        response.pipe(file); //save to file
-        
-        file.on('finish', () => {
-          file.close(resolve); // close() is async, call resolve after close completes.
-          fs.rename(tmpFileDestiny, filepath, (err)=>{
-            if (err) throw err;
-          })
-        });
-      }).on('error', (err) => {
-        fs.unlink(tmpFileDestiny); // Delete the file async.
-        reject(false);
-      });
-      request.write(postOptionsString); //send post parameters
-      request.end();
-    });
-  });
-}
