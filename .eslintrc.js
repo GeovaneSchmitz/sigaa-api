94cdcbf3a48fc0b4264faf6dcea8acf81870aea3
Geovane Schmitz <contato@geovanems.com.br>
2020-01-16 17:31:51 -0300

    Adicionado Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  extends: [
-    'eslint:recommended',
-    'standard'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 2018
-  },
-  rules: {
-  }
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true
+    },
+    "plugins": ['prettier'],
+    "extends": ["plugin:prettier/recommended"],
+        'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'prettier/prettier': 'error',
+    }
 }
