9a5405422067893ba956c2a34340960945f0d99f
Geovane Schmitz <contato@geovanems.com.br>
2020-01-16 22:01:40 -0300

    Adicionado Metodo para baixar foto do perfil

--- libs/student/sigaa-account-student.js
+++ libs/student/sigaa-account-student.js
@@ -1,6 +1,10 @@
 const Cheerio = require('cheerio')
+const fs = require('fs')
+const https = require('https')
+const path = require('path')
 const SigaaAccount = require('../common/sigaa-account')
 const SigaaClassStudent = require('./sigaa-class-student')
+const SigaaErrors = require('../common/sigaa-errors')
 class SigaaAccountStudent extends SigaaAccount {
   /**
    * Get all classes
@@ -8,9 +12,9 @@ class SigaaAccountStudent extends SigaaAccount {
    * @returns {Promise.<Array.<SigaaClassStudent>>}
    * @async
    */
-  getClasses (allPeriods) {
-    return this._get('/sigaa/portais/discente/turmas.jsf')
-      .then(page =>
+  getClasses(allPeriods) {
+    return this._get('/sigaa/portais/discente/turmas.jsf').then(
+      (page) =>
         new Promise((resolve, reject) => {
           const $ = Cheerio.load(page.body, {
             normalizeWhitespace: true
@@ -40,21 +44,127 @@ class SigaaAccountStudent extends SigaaAccount {
                 const classData = {}
                 const fullname = this._removeTagsHtml(cellElements.eq(0).html())
                 classData.title = fullname.slice(fullname.indexOf(' - ') + 3)
-                classData.abbreviation = fullname.slice(0, fullname.indexOf(' - '))
-                classData.numberOfStudents = this._removeTagsHtml(cellElements.eq(2).html())
-                classData.schedule = this._removeTagsHtml(cellElements.eq(4).html())
+                classData.abbreviation = fullname.slice(
+                  0,
+                  fullname.indexOf(' - ')
+                )
+                classData.numberOfStudents = this._removeTagsHtml(
+                  cellElements.eq(2).html()
+                )
+                classData.schedule = this._removeTagsHtml(
+                  cellElements.eq(4).html()
+                )
                 classData.period = period
-                classData.form = this._extractJSFCLJS(buttonClassPage.attr('onclick'), $)
+                classData.form = this._extractJSFCLJS(
+                  buttonClassPage.attr('onclick'),
+                  $
+                )
                 classData.id = classData.form.postValues['idTurma']
-                listClasses.push(new SigaaClassStudent(classData, this._sigaaSession))
+                listClasses.push(
+                  new SigaaClassStudent(classData, this._sigaaSession)
+                )
               }
             }
           }
           resolve(listClasses)
-        }))
+        })
+    )
+  }
+  /**
+   * Download user profile picture, save in filepath
+   * File path can be a directory
+   * @param {String} basepath Path to save the image
+   * @returns {String} Full filepath of image
+   * @throws {SIGAA_USER_HAS_NO_PICTURE} If the user has no  picture
+   * @throws {SIGAA_FILEPATH_NOT_EXISTS} If filepath isn't a directory or file
+   * @throws {SIGAA_UNEXPECTED_RESPONSE} If receive a unexpected response
+   */
+  async downloadProfilePicture(basepath) {
+    const page = await this._get('/sigaa/mobile/touch/menu.jsf').then(
+      (page) => {
+        return this._checkPageStatusCodeAndExpired(page)
+      }
+    )
+    const $ = Cheerio.load(page.body, {
+      normalizeWhitespace: true
+    })
+    const pictureElement = $('div[data-role="fieldcontain"] img')
+    if (pictureElement.length === 0) {
+      throw new Error(SigaaErrors.SIGAA_USER_HAS_NO_PICTURE)
+    }
+    const pictureSrc = pictureElement.attr('src')
+    if (pictureSrc.includes('/img/avatar.jpg')) {
+      throw new Error(SigaaErrors.SIGAA_USER_HAS_NO_PICTURE)
+    }
+    const fileStats = fs.lstatSync(basepath)
+    if (!(fileStats.isDirectory() || fileStats.isFile())) {
+      reject(new Error(SigaaErrors.SIGAA_FILEPATH_NOT_EXISTS))
+    }
+    const pictureURL = new URL(pictureSrc, this._sigaaSession.url)
+    const options = this._makeRequestBasicOptions('GET', pictureURL)
+
+    // makes request
+    return new Promise((resolve, reject) => {
+      const request = https.request(options, (response) => {
+        if (response.statusCode !== 200) {
+          reject(new Error(SigaaErrors.SIGAA_UNEXPECTED_RESPONSE))
+        }
+        try {
+          let filepath
+          if (fileStats.isDirectory()) {
+            if (response.headers['content-disposition']) {
+              const filename = response.headers['content-disposition']
+                .replace(/([\S\s]*?)filename="/gm, '')
+                .slice(0, -1)
+              const contentType = response.headers['content-type']
+              let extension
+              switch (contentType) {
+                case 'image/png':
+                  extension = '.png'
+                  break
+                case 'image/jpeg':
+                  extension = '.jpg'
+                  break
+                case 'image/gif':
+                  extension = '.gif'
+                  break
+                default:
+                  extension = ''
+              }
+              filepath = path.join(basepath, filename + extension)
+            } else {
+              reject(new Error(SigaaErrors.SIGAA_UNEXPECTED_RESPONSE))
+            }
+          } else {
+            filepath = basepath
+          }
+
+          const file = fs.createWriteStream(filepath)
+          response.pipe(file) // save to file
+
+          file.on('finish', () => {
+            file.close() // close() is async, call resolve after close completes.
+            resolve(filepath)
+          })
+          response.on('error', (err) => {
+            file.close(file)
+            fs.unlinkSync(filepath)
+            reject(err)
+          })
+          file.on('error', (err) => {
+            file.close()
+            fs.unlinkSync(filepath)
+            reject(err)
+          })
+        } catch (err) {
+          reject(err)
+        }
+      })
+      request.end()
+    })
   }
 
-  async getUsername () {
+  async getUsername() {
     const page = await this._get('/sigaa/portais/discente/discente.jsf')
     if (page.statusCode === 200) {
       const $ = Cheerio.load(page.body, {
